9205477c863865470cf098a8dbe6f38b4b659721
Francisco Sans <franjaviersans@gmail.com>
2015-03-19 15:52:43 -0430

    Little bug fixed

--- cudaIntersection/kernel.cuh
+++ cudaIntersection/kernel.cuh
@@ -23,15 +23,14 @@
 
 #define EPSILON 0.000001
 #define CROSS(dest, v1, v2) \
-	dest.x = v1.x*v2.x - v1.x*v2.x; \
-	dest.y = v1.y*v2.y - v1.y*v2.y; \
-	dest.z = v1.z*v2.z - v1.z*v2.z;
+	dest.x = v1.y*v2.z - v1.z*v2.y; \
+	dest.y = v1.z*v2.x - v1.x*v2.z; \
+	dest.z = v1.x*v2.y - v1.y*v2.x; 
 #define DOT(v1, v2) (v1.x*v2.x+v1.y*v2.y+v1.z*v2.z)
 #define SUB(dest, v1, v2) \
 	dest.x = v1.x - v2.x; \
 	dest.y = v1.y - v2.y; \
 	dest.z = v1.z - v2.z; 
-
 #define MULT(dest, mat,p) \
 	dest.x = mat[0] * p.x + mat[1] * p.y + mat[2] * p.z + mat[3] * 1.0f; \
 	dest.y = mat[4] * p.x + mat[5] * p.y + mat[6] * p.z + mat[7] * 1.0f; \
